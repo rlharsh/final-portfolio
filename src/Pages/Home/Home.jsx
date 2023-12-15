e8b564d84f0f091fd82a3a4ea60f11f34d8c842a
rlharsh <ronald.harsh@protonmail.com>
2023-12-14 21:28:43 -0600

    Added cheeky text.

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -6,7 +6,10 @@ import "./home.css";
 const Home = () => {
   return (
     <div className="hero-container flex center no-select">
-      <p>This is your homepage.</p>
+      <p>
+        This is my homepage, there are many like it, but this one is mine.
+        Select an option above to get started.
+      </p>
     </div>
   );
 };

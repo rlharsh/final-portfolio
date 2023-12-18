c6cc41b0dafdbf0655070e8e1be44162b85dc484
rlharsh <ronald.harsh@protonmail.com>
2023-12-17 19:59:14 -0600

    Update

--- src/Helpers/contentfulClient.js
+++ src/Helpers/contentfulClient.js
@@ -1,5 +1,11 @@
 import { createClient } from "contentful";
 
+if (!import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN) {
+  throw new Error(
+    "Contentful access token is not defined in environment variables."
+  );
+}
+
 const contentfulClient = createClient({
   space: "1v983nokqj8p",
   accessToken: import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN,

86df5bc10390583ecef00cc3d7ee294aa7929ec5
rlharsh <ronald.harsh@protonmail.com>
2023-12-17 19:57:28 -0600

    Update JS

--- src/Helpers/contentfulClient.js
+++ src/Helpers/contentfulClient.js
@@ -2,7 +2,7 @@ import { createClient } from "contentful";
 
 const contentfulClient = createClient({
   space: "1v983nokqj8p",
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+  accessToken: import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN,
 });
 
 export default contentfulClient;
